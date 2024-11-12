45ac2f8b76f70f1ddd763c913915a32ec6774ca2
zyadRA <zyadmohmd009@gmail.com>
2024-11-12 03:32:01 +0300

    Created create quiz page(incomplete)

--- src/QuizMaker/Create/CreateQuizPage.jsx
+++ src/QuizMaker/Create/CreateQuizPage.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import QuestionCard from './QuestionCard';
+import './CreateQuizPage.css';
+import { useNavigate } from 'react-router-dom';
 
 function CreateQuizPage(){
-    return <h1>Create Quiz Page</h1>;
-}
+    const navigate = useNavigate();
+    function handleExitClick(){navigate('/quiz-maker-dashboard')}
+    return(
+        <div className='createQuiz'>
+            <div className='sidebar'>
+                <button className='exitButton' onClick={handleExitClick}>Exit</button>
+                <ul>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+                <button className='addQuestionButton'>Add Question</button>
+            </div>
+
+            <div className='mainPage'>
+                <div className='topbar'>
+                    <input type="text" className="quizNameInput" placeholder="Enter Quiz Name"/>
+                    <button className='publishButton'>Publish</button>
+                </div>
+                <div className='questinCards'>
+                    <QuestionCard type='mcq'/>
+                    <QuestionCard type='tf'/>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
 
 export default CreateQuizPage;
