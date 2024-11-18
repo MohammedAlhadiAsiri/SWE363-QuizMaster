94e981b4fbbfd64e67531f18be8a9eb0ad3dc906
Feras <s202038040@kfupm.edu.sa>
2024-11-18 23:55:47 +0300

    Updating the import source for QuizzesContainer

--- src/pages/MakerPage.jsx
+++ src/pages/MakerPage.jsx
@@ -1,6 +1,6 @@
 //This is QuizMakerPage.jsx
 import React from 'react'
-import QuizzesContainer from '../QuizMaker/Dashboard/QuizzesContainer'
+import QuizzesContainer from './QuizContainerMakerPage'
 import Navbar from '../Navbar';
 import '../QuizMaker/Dashboard/QuizMakerDashboard.css'
 function QuizMakerPage(){
